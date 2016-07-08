52cfc2c7dcea32f734e4c88bba3e04f3e12ffb59
andriyt85 <andriyt85@ukr.net>
2016-07-08 15:29:01 +0300

    Add files via upload

--- Task2/script.js
+++ Task2/script.js
@@ -1,10 +1,23 @@
-(function () {
+(function (j) {
 	var f = [0,1];
+	f.length = j;
 
-
-	for (i = 2; i < 21; i++){
+	for (i = 2; i < f.length; i++){
 		f[i] = f[i - 1] + f[i - 2] 
-		document.write(f[i] + " ");
+		document.write(f[i] + " ");3
+
+		var str = f[i].toString();
+		
+		/*if ((str[0] % 2) === 0){
+			console.log("Main even" + " " + str)
+		} else if ((str[0] % 2) !== 0) {
+			console.log("Main odd" + " " + str)
+		} else if ((f[i] % 2) === 0) {
+			console.log("Secondary even" + " " + f[i])
+		} /*else */if ((f[i] % 2) !== 0) {
+			console.log("Secondary odd" + " " + f[i])
+		} 
 	}
 	
-}());
+
+}(21));
