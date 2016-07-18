cfbf38496a089c1df5dd2483a4c5870fd54ee72d
andriyt85 <andriyt85@ukr.net>
2016-07-18 18:10:40 +0300

    Update app.js

--- ss/app.js
+++ ss/app.js
@@ -1,3 +1,10 @@
-
-var app = angular.module('myApp', []);
-
+(function () {
+    'use strict';
+    var mainURL = "http://dtapi.local/";
+     angular.module('myApp', [])
+         .constant('myAppConst', {
+            loginURL: mainURL + 'login/index',
+            logOutURL: mainURL + 'login/logout',
+            isLogURL: mainURL + 'login/isLogged'
+         });
+})();
