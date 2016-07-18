fc37eb27884964c59450305f08fe35804df51956
andriyt85 <andriyt85@ukr.net>
2016-07-18 18:11:05 +0300

    Update loginCtrl.js

--- ss/loginCtrl.js
+++ ss/loginCtrl.js
@@ -1,18 +1,27 @@
-app.controller('myCtrl', function ($scope, $http) {
-       var user = {};
-    function myLoginFunction () {
-       $http({
-           method: 'POST',
-           url: 'http://dtapi.local/login/index',
-           data: $scope.user,
-           headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-       })
-           .success(function (data,status,headers,config) {
-                   console.log(data);
-               })
-           .error(function (data,status,headers,config) {
-                   console.log('error');
-               })
-   }
+(function() {
+    'use strict';
+    angular.module('myApp')
+        .controller('loginCtrl',['$http', function ($http) {
+            var vm = this;
+            vm.user = {
+                username: "",
+                password: ""
+            };
+            vm.enter = enter;
+            function enter() {
+                $http({
+                    method: 'POST',
+                    url: myAppConst.loginURL,
+                    data: JSON.stringify(vm.user),
+                    headers : {'Content-Type': 'application/json'}
+                })
+                    .success(function (data,status,headers,config) {
+                        console.log('success' + data);
+                    })
+                    .error(function (data,status,headers,config) {
+                        console.log('error');
+                    })
 
-});
+            }
+        }]);
+})();
